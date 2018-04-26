8ebed4e8df17f783014e3480e3ac69c1ac14110a
cuiqiang <cuiqiang@ling.ai>
2018-04-26 21:22:10 +0800

    add log

--- testffmpeg/app/src/main/cpp/native-lib.cpp
+++ testffmpeg/app/src/main/cpp/native-lib.cpp
@@ -1,31 +1,13 @@
-/*******************************************************************************
-**                                                                            **
-**                     Jiedi(China nanjing)Ltd.                               **
-**	               创建：夏曹俊，此代码可用作为学习参考                       **
-*******************************************************************************/
-
-/*****************************FILE INFOMATION***********************************
-**
-** Contact       : xiacaojun@qq.com
-**        博客   : http://blog.csdn.net/jiedichina
-**		视频课程 : 网易云课堂	http://study.163.com/u/xiacaojun		
-				   腾讯课堂		https://jiedi.ke.qq.com/				
-				   csdn学院		http://edu.csdn.net/lecturer/lecturer_detail?lecturer_id=961	
-**                 51cto学院	http://edu.51cto.com/lecturer/index/user_id-12016059.html	
-** 				   老夏课堂		http://www.laoxiaketang.com #开发中预计2018年6月前上线
-**                 
-**   微信公众号  : jiedi2007
-**		头条号	 : 夏曹俊
-**
-*******************************************************************************/
-
 
 #include <jni.h>
 #include <string>
+#include "android/log.h"
 extern "C"{
     #include <libavcodec/avcodec.h>
 }
-
+#define TAG "jni"
+#define LOGD(fmt, args...) __android_log_print(ANDROID_LOG_DEBUG, TAG, fmt, ##args)
+#define LOGE(fmt, args...) __android_log_print(ANDROID_LOG_ERROR, TAG, fmt, ##args)
 extern "C"
 JNIEXPORT jstring
 
@@ -35,5 +17,6 @@ Java_aplay_testffmpeg_MainActivity_stringFromJNI(
         jobject /* this */) {
     std::string hello = "Hello from C++ ";
     hello += avcodec_configuration();
+    LOGD("log");
     return env->NewStringUTF(hello.c_str());
 }
