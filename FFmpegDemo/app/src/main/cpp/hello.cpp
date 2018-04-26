2b72a52f33e44ca2af33e520eccd2cb3982ef6de
cuiqiang <cuiqiang@ling.ai>
2018-04-26 20:23:02 +0800

    add ffmpeg

--- FFmpegDemo/app/src/main/cpp/hello.cpp
+++ FFmpegDemo/app/src/main/cpp/hello.cpp
@@ -2,11 +2,30 @@
 // Created by cuiqiang on 17-11-28.
 //
 #include "jni.h"
-extern  "C"
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include "libavformat/avformat.h"
+}
+extern "C"
 
 JNIEXPORT jstring JNICALL Java_cuiqiang_ling_ai_cmakedemo_MainActivity_getStr
-        (JNIEnv *env,jobject jobject){
+        (JNIEnv *env, jobject jobject) {
 
-    return env->NewStringUTF("hello world from cpp");
+    avcodec_register_all();
+
+    avformat_network_init();
+
+    AVFormatContext *avFormatContext = NULL;
+
+    char path[] = "/sdcard/1080.mp4";
+
+    int result = avformat_open_input(&avFormatContext, path, 0, 0);
+    if (result == 0) {
+
+    } else {
+
+    }
+    return env->NewStringUTF("open success");
 }
 
